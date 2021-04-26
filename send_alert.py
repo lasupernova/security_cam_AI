0faf2b2b9d696df4589ec16330fe0fc302976c55
lasupernova <gabrilasuno@gmail.com>
2021-04-26 22:13:42 +0200

    added image attachment functionality

--- send_alert.py
+++ send_alert.py
@@ -3,17 +3,21 @@ import email, smtplib, ssl
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 from dotenv import load_dotenv  
 load_dotenv()                    
+
+import imghdr  # And imghdr to find the types of our images
 
 import os 
 
 # get email password from .env-file
 password = os.environ.get('PASSWORD')
 
-subject = ">>>OMG, la camara captura a alguien entrando a tu casa!!!!!"
+subject = "FOTO: OMG, la camara captura a alguien entrando a tu casa!!!!!"
 body = "Aqui esta la foto del ladron..."
 sender_email = os.environ.get("SENDER")
 # receiver_email = "karina@mosicom.de"
@@ -21,23 +25,25 @@ receiver_email = os.environ.get("RECIPIENT")
 
 
 # Create a multipart message and set headers
-message = MIMEMultipart()  #multipart in order to attach picture
+message = EmailMessage()  #multipart in order to attach picture
 message["From"] = sender_email
 message["To"] = receiver_email
-message["Subject"] = subject
+message.preamble = subject
 # message["Bcc"] = receiver_email  # Recommended for mass emails
 
 # # Add body to email
 # message.attach(MIMEText(body, "plain"))
-
+ 
 filename = f"media{os.sep}test.jpg"  # In same directory as script
-img_data = open(filename, 'rb').read()
+with open(filename, 'rb') as fp:
+    img_data = fp.read()
+# image = MIMEImage(img_data, name=os.path.basename(filename))
+message.add_attachment(img_data, maintype='image',
+                                 subtype=imghdr.what(None, img_data))
 
-
-text = "This is a test!"
 
 # Log in to server using secure context and send email
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+    server.send_message(message)
