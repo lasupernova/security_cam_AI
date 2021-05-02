0d96082274d21a590ad04d5754e5dcdbd9944cf1
lasupernova <gabrilasuno@gmail.com>
2021-05-02 17:45:05 +0200

    added function to recognize blurry images

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import datetime
+import cv2
 
 def pretty_print_timedelta(delta):
     """
@@ -19,3 +20,32 @@ def pretty_print_timedelta(delta):
         return f"{hh}h:{mm}m:{ss}s"
     else:
         return "More than 24 hours!"
+
+
+def detect_blurry_img(image=None):
+    """
+    Detects blurry images by computing the Laplacian of the image and returning the focus
+	measure. This measure is the variance of the Laplacian (using cv2.Laplacian())
+
+    Parameters:
+        image (numpy.array) - image in array-form
+
+    Returns:
+        bool - 1 (blurry) or 0 (not blurry)
+    """
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  #load image from array and convert image to grayscale 
+
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F).var() 
+
+    if laplacian < 250:  # image will be considered blurry for a Laplacian < 250
+        return 1  #image is blurry
+    else:  
+        return 0  #not blurry 
+
+
+
+if __name__ == "__main__":
+    detect_blurry_img()
+
+    
